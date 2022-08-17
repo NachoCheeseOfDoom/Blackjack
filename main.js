b39db9dc2b3049641854958065fdf52f2b500715
NachoCheeseOfDoom <85528067+NachoCheeseOfDoom@users.noreply.github.com>
2022-08-17 09:46:31 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,91 +1,72 @@
+let player = {
+    name: 'Nacho',
+    chips: 165
+}
+let cards = [];
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
 
-// let cards = [];
-// let sum = 0
-// let hasBlackJack = false
-// let isAlive = false
-// let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el");
 
-// let messageEl = document.getElementById("message-el")
-// let sumEl = document.getElementById("sum-el")
-// let cardsEl = document.getElementById("cards-el")
-// let playerEl = document.getElementById("player-el");
+playerEl.textContent = `${player.name}: $${player.chips}`;
 
-// let player = {
-//     name: 'Nacho',
-//     chips: 165
-// }
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1;
+    if (randomNumber > 10) {
+        return 10;
+    } else if (randomNumber === 1) {
+        return 11;
+    }
+    else {
+        return randomNumber;
+    }
 
-
-// playerEl.textContent = `${player.name}: $${player.chips}`;
-
-
-
-// function getRandomCard() {
-//     let randomNumber = Math.floor(Math.random() * 13) + 1;
-//     if (randomNumber > 10) {
-//         return 10;
-//     } else if (randomNumber === 1) {
-//         return 11;
-//     }
-//     else {
-//         return randomNumber;
-//     }
-
-// }
-
-
-// function startGame() {
-//     isAlive = true;
-//     // Generate two random numbes
-//     let firstCard = getRandomCard();
-//     let secondCard = getRandomCard();
-
-//     // Re-assign the cards and sum variables so that the game can start
-//     cards = [firstCard, secondCard];
-//     sum = firstCard + secondCard
-
-//     renderGame()
-// }
-
-// function renderGame() {
-//     cardsEl.textContent = "Cards: "
-//     for (let i = 0; i < cards.length; i++) {
-//         cardsEl.textContent += cards[i] + " "
-//     }
-
-//     sumEl.textContent = "Sum: " + sum
-//     if (sum <= 20) {
-//         message = "Do you want to draw a new card?"
-//     } else if (sum === 21) {
-//         message = "You've got Blackjack!"
-//         hasBlackJack = true
-//     } else {
-//         message = "You're out of the game!"
-//         isAlive = false
-//     }
-//     messageEl.textContent = message
-// }
-
-
-// function newCard() {
-//     if (isAlive === true && hasBlackJack === false) {
-//         let card = getRandomCard();
-//         sum += card
-//         // Push the card to the cards array
-//         cards.push(card)
-//         console.log(cards)
-//         renderGame()
-//     }
-// }
-
-
-
-let airbnb = {
-    title: 'San Fusto Abbey',
-    price: 173,
-    available: true,
-    rooms: ['1 person', 'family size', 'kitchen']
 }
 
-console.log(`${airbnb.title} cost: $${airbnb.price}`);
+function startGame() {
+    isAlive = true;
+    // Generate two random numbes
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
 
+    // Re-assign the cards and sum variables so that the game can start
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard
+
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard();
+        sum += card
+        // Push the card to the cards array
+        cards.push(card)
+        console.log(cards)
+        renderGame()
+    }
+}
